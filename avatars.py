5e28cf2279ec77a3c817b01441a2c8e498aa9e6c
akashg001 <akash.gupta4701@gmail.com>
2023-04-15 17:55:45 +0530

    payment

--- avatars.py
+++ avatars.py
@@ -4,7 +4,7 @@ from io import BytesIO
 from PIL import Image
 
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 # response = openai.Image.create(
 #   prompt="a white siamese cat",
@@ -12,6 +12,7 @@ openai.api_key = "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"
 #   size="1024x1024"
 # )
 # image_url = response['data'][0]['url'] # type: ignore
+
 
 def avatar(prompt):
     response = openai.Image.create(
@@ -22,6 +23,7 @@ def avatar(prompt):
 
     # Convert the image data to a Pillow image object
 
-    img_data = requests.get(response['data'][0]['url'])
-    return img_data
+    img_data = requests.get(response['data'][0]['url']).content
+    return response
     
+# avatar("2D monkey avatar with goggles,jewellery, cap and hoodie")
--- home.py
+++ home.py
@@ -1,37 +1,57 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 from avatars import avatar
 import os
 from PIL import Image
 from io import BytesIO
+import json
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('form.html')
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['post'])
 def submit():
+    global pay
+    pay = 0
     prompts = ""
-    if request.form['dimension']:
-        prompts = request.form['dimension']
+    dim = request.form.get('dimension')
+    ann = request.form.get('animal')
+    cap = request.form.get('cap')
+    jwellery = request.form.get('jwellery')
+    goggles = request.form.get('goggles')
+    dress = request.form.get('dress')
+    mood = request.form.get('mood')
     
-    if request.form['animal']:
-        prompts += request.form['animal'] + " Avatar"    
-    if request.form['cap']:
-        prompts += "with "+request.form['cap']
-    if request.form['jwellery']:
-        prompts += ", "+request.form['jwellery']
-    if request.form['goggles']:
-        prompts += ", "+request.form['goggles']
-    if request.form['dress']:
-        prompts += "wearing "+request.form['dress']
-    if request.form['mood']:
-        prompts += "in "+request.form['dress']+" mood"
+    print(cap)
+    if dim is not None:
+        prompts = dim
+    if ann is not None:
+        prompts = prompts+" "+ann + " Avatar " 
+    print(prompts)
+    prompts = prompts+" with "   
+    if cap is not None:
+        prompts = prompts + cap
+        pay+=10
+    print(prompts)
+    if jwellery is not None:
+        prompts = prompts+", "+jwellery
+        pay+=20
+    print(prompts)
+    if goggles is not None:
+        prompts = prompts+", "+goggles
+        pay+=5
+    if dress is not None:
+        prompts = prompts+", wearing "+dress
+        pay+=15
+    if mood is not None:
+        prompts = prompts+", "+mood+" mood"
+    print(prompts)
     img_data = avatar(prompts)
-    img = img_data.content
-    #img = Image.open(BytesIO(content))
-    return img
+    # img_data = img_data.choices[0].text
+    return json.dumps({'status':'OK','image_url' : img_data['data'][0]['url']})
+
     # Display or save the image
     # img.show()
 
@@ -41,3 +61,17 @@ def mint_now():
     img_url = request.form['img']
     # function to do further process of minting
     return img_url
+
+
+@app.route("/payment", methods=['GET', 'POST'])
+def index():
+      data = pay
+      return redirect(f"/{data}")
+
+@app.route("/<upi>")
+def next(upi):
+  return render_template("index.html", upi=upi)
+
+
+if __name__ == "__main__":
+    app.run()
