d04ee0539ba8fb8660061937e7988e91114e6601
kumaramit123 <amit.kumar@acem.edu.in>
2023-04-15 12:12:22 +0530

    updated contract

--- smartpy.py
+++ smartpy.py
@@ -12,7 +12,7 @@ class AvatarNFT(sp.Contract):
 
     # Define a method to mint a new NFT
     @sp.entry_point
-    def mint(self, metadata):
+    def mint_avatar(self, metadata):
         sp.verify(self.data.owner == sp.sender, "Only the owner can mint NFTs")
         nft_id = self.data.nft_count
         self.data.nft_count += 1
@@ -28,6 +28,19 @@ class AvatarNFT(sp.Contract):
         sp.set_type(nft, sp.TNat)
         sp.set_type(self.data.nft_count, sp.TNat)
         sp.set_type(self.data.nft_metadata, sp.TMap(sp.TNat, sp.TString))
+
+    
+    @sp.entry_point
+    def transfer(self, params):
+        from_token_id = params.tokenId
+        to_address = params.toAddress
+        sp.verify(sp.sender == self.data.tokenOwners[from_token_id], message="Only the token owner can transfer tokens")
+        self.data.tokenOwners[from_token_id] = to_address
+        owner_token_count_from = sp.as_int(self.data.ownerTokenCount.get(sp.sender, 0))
+        owner_token_count_to = sp.as_int(self.data.ownerTokenCount.get(to_address, 0))
+        self.data.ownerTokenCount[sp.sender] = owner_token_count_from - 1
+        self.data.ownerTokenCount[to_address] = owner_token_count_to + 1
+
 
 # Define the main smart contract
 class Main(sp.Contract):
